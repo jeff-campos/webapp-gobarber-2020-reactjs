d650aad1b705a03e8ee40417b8c8aa146f9c7c72
Admin <admin@jeff-campos.local>
2020-12-14 13:18:35 -0300

    Feat(Tooltip|Comonent): create element

--- src/components/Input/styles.tsx
+++ src/components/Input/styles.tsx
@@ -1,8 +1,10 @@
 import styled, { css } from 'styled-components';
+import Tooltip from '../Tooltip';
 
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrorRed: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -28,6 +30,12 @@ export const Container = styled.div<ContainerProps>`
   }
 
   ${props =>
+    props.isErrorRed &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${props =>
     props.isFocused &&
     css`
       color: #ff9000;
@@ -46,5 +54,23 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
