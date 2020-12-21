f60077784902ac3c12623dbf5a34fd1510860906
Admin <admin@jeff-campos.local>
2020-12-21 18:07:35 -0300

    Feat(Route): creating custom route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './route';
 
 import SingIn from '../pages/SignIn';
 import SingUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SingIn} />
     <Route path="/signup" exact component={SingUp} />
+
+    <Route path="/dashboard" exact component={Dashboard} isPrivate />
   </Switch>
 );
 
